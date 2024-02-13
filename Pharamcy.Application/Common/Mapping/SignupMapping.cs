fcc73223630fb91c5dcf25c9ebee62d05b4062ee
Hossam Samy <hossamsamy234@gmail.com>
2024-02-13 14:28:50 +0200

    RemoveComment

--- Pharamcy.Application/Common/Mapping/SignupMapping.cs
+++ Pharamcy.Application/Common/Mapping/SignupMapping.cs
@@ -1,22 +1,22 @@
-﻿//using Mapster;
-//using Microsoft.AspNetCore.Routing.Constraints;
-//using Pharamcy.Application.Features.Signup.Commands;
-//using Pharamcy.Domain.Identity;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Mapster;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Pharamcy.Application.Features.Signup.Commands;
+using Pharamcy.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Pharamcy.Application.Common.Mapping
-//{
-//    public class SignupMapping : IRegister
-//    {
-//        public void Register(TypeAdapterConfig config)
-//        {
-//            config.NewConfig<SignupCommand,ApplicationUser>();
-          
+namespace Pharamcy.Application.Common.Mapping
+{
+    public class SignupMapping : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<SignupCommand, ApplicationUser>();
 
-//        }
-//    }
-//}
+
+        }
+    }
+}
