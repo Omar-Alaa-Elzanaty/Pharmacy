61491f31d009f0e2a4bd065beab777e11ab67de9
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-02-14 18:49:46 +0200

    Add appsettings

--- Pharamacy.API/Program.cs
+++ Pharamacy.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Pharamcy.Application.extention;
 using Pharamcy.Application.Interfaces;
 using Pharamcy.Infrastructure.Extention;
@@ -23,6 +24,34 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement{
+                {
+                  new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+                }
+            });
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
