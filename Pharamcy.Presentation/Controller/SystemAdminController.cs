56f7ab6db0de6afab7f7b4726292bab16faf2577
Hossam Samy <hossamsamy234@gmail.com>
2024-02-19 17:22:53 +0200

    GetAdminOrSystemAdminByIdEndPoint

--- Pharamcy.Presentation/Controller/SystemAdminController.cs
+++ Pharamcy.Presentation/Controller/SystemAdminController.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Pharamcy.Application.Features.SystemAdmin.Queries.GetAllSystemAdminAndAdmin;
+using Pharamcy.Application.Features.SystemAdmin.Queries.GetSystemAdminOrAdminById;
+using Pharamcy.Shared;
 
 namespace Pharamcy.Presentation.Controller
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
+    [Authorize(Roles =SystemRoles.SystemAdmin)]
     public class SystemAdminController:ApiControllerBase
     {
         private readonly IMediator _mediator;
@@ -25,6 +29,11 @@ namespace Pharamcy.Presentation.Controller
         public async Task<ActionResult<GetAllSystemAdminOrAdminDto>> GetAll(string id, string role)
         {
             return Ok(await _mediator.Send(new GetAllSystemAdminOrAdmin(role,id)));
+        }
+        [HttpGet]
+        public async Task<ActionResult<GetSystemAdminOrAdminByIdDto>> GetById(string id)
+        {
+            return Ok(await _mediator.Send(new GetSystemAdminOrAdminByIdQuery(id)));
         }
     }
 }
