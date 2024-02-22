d7ce0ac354f0c3514fb96d4c350dc0514414c181
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-02-22 14:10:49 +0200

    Add Images

--- Pharamcy.Application/Features/Authentication/Signup/Commands/CreateSystemAdminAndAdmin/CreateSystemAdminAndAdminCommand.cs
+++ Pharamcy.Application/Features/Authentication/Signup/Commands/CreateSystemAdminAndAdmin/CreateSystemAdminAndAdminCommand.cs
@@ -19,7 +19,7 @@ namespace Pharamcy.Application.Features.Authentication.Signup.Commands.CreateSys
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string NationalId { get; set; }
-        public IFormFile ImageProfile { get; set; }
+        public IFormFile? ImageProfile { get; set; }
         public string Role { get; set; }
     };
     internal class CreateSystemAdminAndAdminCommandHandler : IRequestHandler<CreateSystemAdminAndAdminCommand, Response>
