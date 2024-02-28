2b68af23bf6e235be03f82c0401113421e617ea7
Hossam Samy <hossamsamy234@gmail.com>
2024-02-28 16:51:54 +0200

    fixingLogin

--- Pharamcy.Application/Features/Authentication/Login/Queries/GetLoginDto.cs
+++ Pharamcy.Application/Features/Authentication/Login/Queries/GetLoginDto.cs
@@ -10,6 +10,9 @@ namespace Pharamcy.Application.Features.Authentication.Login.Queries
     {
         public string Id { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PharmacyName { get; set; }
         public string Role { get; set; }
         public string Token { get; set; }
         public string? ImageUrl { get; set; }
