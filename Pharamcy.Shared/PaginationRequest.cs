88ea024ad04e90bbdf2525b3ef2be6009b4a6c31
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-03-15 13:18:15 +0200

    add paginationRequest

---
+++ Pharamcy.Shared/PaginationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharamcy.Shared
+{
+    public record PaginationRequest
+    {
+        public string? KeyWord { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set;}
+    }
+}
