d48ec3193e8e1372934378d35c2ab8c5fe7aff7c
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-04-06 13:24:47 +0200

    Add Supplier Type

---
+++ Pharamcy.Application/Features/MedicalTypes/Queries/GetAllMedicalTypesQueryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharamcy.Application.Features.MedicalTypes.Queries
+{
+    public class GetAllMedicalTypesQueryDto
+    {
+        public string Name { get; set; }
+    }
+}
