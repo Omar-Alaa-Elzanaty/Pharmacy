d01b35ff5718580c3cd14a9aea2adb18a8822ed0
ahemmahersebap <ahemmahersebap@gmail.com>
2024-04-14 17:12:52 +0200

    Adding Offer Model

---
+++ Pharamcy.Domain/Models/Offer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharamcy.Domain.Models
+{
+	public class Offer:BaseEntity
+	{
+        public int Amount { get; set; }
+        public double Discount_percentage { get; set; }
+        public virtual Medicine Medicine { get; set; }
+
+	}
+}
