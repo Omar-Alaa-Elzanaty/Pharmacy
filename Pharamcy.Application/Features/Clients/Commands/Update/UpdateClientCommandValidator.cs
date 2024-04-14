dc0b9545e0e25fd6e720935cf022c519dc6e4fea
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-04-14 13:48:55 +0200

    UpdateClientValidation

--- Pharamcy.Application/Features/Clients/Commands/Update/UpdateClientCommandValidator.cs
+++ Pharamcy.Application/Features/Clients/Commands/Update/UpdateClientCommandValidator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Pharamcy.Application.Features.Clients.Commands.Update
 {
-    internal class UpdateClientCommandValidator
+    public class UpdateClientCommandValidator:AbstractValidator<UpdateClientCommand>
     {
+        public UpdateClientCommandValidator()
+        {
+            RuleFor(x => x.PhoneNumber).Length(11);
+            RuleFor(x => x.PhoneNumber).MaximumLength(20);
+            RuleFor(x => x.Email).EmailAddress();
+        }
     }
 }
